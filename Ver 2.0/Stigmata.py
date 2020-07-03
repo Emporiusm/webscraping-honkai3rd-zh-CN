1416f441a4ead3896257c88a3aab236ebda8b354
Eden MUI <emporius@Edens-MacBook-Pro.local>
2020-07-04 01:59:08 +0800

    --- Auto-Git Commit ---

--- Ver 2.0/Stigmata.py
+++ Ver 2.0/Stigmata.py
@@ -2,14 +2,31 @@
 class StigmataSet:
     """Class for Stigmata Objects"""
 
+    setability={
+        2:None,
+        3:None
+    }
 
     def __init__(self,name,setability2,setability3):
         self.name           = name
         self.setability2    = setability2
         self.setability3    = setability3
 
-    def getability(self,pieces):
-        ability = [ability for ability in ]
+    def setability(self,pieces,ability):
+
+    def __setattr__
+        self.
+        __setattr__
+        (setability2,ability)
+
+
+    def getability(self,pieces:in
+        setability = {
+            1:None,
+            2:self.setability2,
+            3:self.setability3
+        }
+        return setability.get(pieces)
 
 class Stigmata(StigmataSet):
 
