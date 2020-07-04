8c701c882e8a926faaf36e85a866f68d1fc28601
Eden MUI <emporius@Edens-MacBook-Pro.local>
2020-07-04 19:21:46 +0800

    --- Auto-Git Commit ---

--- Ver 2.0/Valkyrie.py
+++ Ver 2.0/Valkyrie.py
@@ -11,8 +11,6 @@ import urllib.request
 from urllib.request import urlopen
 # %%
 pages = range(100)
-urls    = ['https://3rdguide.com/web/valk/detail?id=' + str(page) for page in pages]
-htmls   = [urlopen(url).read() for url in urls]
-soup    = (BeautifulSoup(html.decode('utf-8'),'html.parser') for html in htmls)
-#%%
-pd.Series(soup)
+urls        = ['https://3rdguide.com/web/valk/detail?id=' + str(page) for page in pages]
+htmls       = [urlopen(url).read() for url in urls]
+souplist    = [BeautifulSoup(html.decode('utf-8'),'html.parser') for html in htmls]
