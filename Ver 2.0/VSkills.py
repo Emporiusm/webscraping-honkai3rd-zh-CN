eba4d17437067a69f878c9a4f823e715242846e7
Eden MUI <emporius@Edens-MacBook-Pro.local>
2020-07-15 23:55:22 +0800

    --- Auto-Git Commit ---

--- Ver 2.0/VSkills.py
+++ Ver 2.0/VSkills.py
@@ -10,6 +10,8 @@ import sys
 import pickle
 import openpyxl
 import os
+import time
+import numpy as np
 
 
 # %%
@@ -30,7 +32,7 @@ class Valkyrie:
     def __init__(self,page,soup=None):
         self.page   = 'https://3rdguide.com/web/valk/detail?id=' + str(page)
         html        = urlopen(self.page).read()
-        self.soup   = BeautifulSoup(html.decode('utf-8'),'html.parser')
+        soup   = BeautifulSoup(html.decode('utf-8'),'html.parser')
 
     def getskills(self):
         df = pd.DataFrame()
@@ -69,7 +71,11 @@ class Valkyrie:
 # %%
 def loadweb(urls):
     htmls       = [urlopen(url).read() for url in urls]
-    souplist    = [BeautifulSoup(html.decode('utf-8'),'html.parser') for html in htmls]
+    souplist    = []
+    for html in htmls:
+        soup = BeautifulSoup(html.decode('utf-8'),'html.parser')
+        souplist.append(soup)
+        time.sleep(np.random.randint(2,5))
     return souplist
 
 
@@ -94,20 +100,20 @@ def loadsoup(souplist_pkl):
 
 # %%
 def soup2skills(souplist):
-    df = pd.DataFrame()
+    df_vskills = pd.DataFrame()
     for soup in souplist:
-        sk        = [sk.get_text()      for sk      in self.soup.findAll('p',    class_='item1')     ]
-        skname    = [skname.get_text()  for skname  in self.soup.findAll('p',    class_='item2')     ]
-        skdesc    = [skdesc.get_text()  for skdesc  in self.soup.findAll('p',    class_='msg')       ]
-        sk1name   = [sk1name.get_text() for sk1name in self.soup.findAll('p',    class_='item1_1')   ]
-        sk1desc   = [sk1desc.get_text() for sk1desc in self.soup.findAll('p',    class_='item1_2')   ]
-        sk1stat   = [sk1stat.get_text() for sk1stat in self.soup.findAll('div',  class_='item3')     ]
-        sk2name   = [sk2name.get_text() for sk2name in self.soup.findAll('p',    class_='item1_1')   ]
-        sk2desc   = [sk2desc.get_text() for sk2desc in self.soup.findAll('p',    class_='item1_2')   ]
-        sk2stat   = [sk2stat.get_text() for sk2stat in self.soup.findAll('div',  class_='item3')     ]
-        sk3name   = [sk3name.get_text() for sk3name in self.soup.findAll('p',    class_='item1_1')   ]
-        sk3desc   = [sk3desc.get_text() for sk3desc in self.soup.findAll('p',    class_='item1_2')   ]
-        sk3stat   = [sk3stat.get_text() for sk3stat in self.soup.findAll('div',  class_='item3')     ]
+        sk        = [sk.get_text()      for sk      in soup.findAll('p',    class_='item1')     ]
+        skname    = [skname.get_text()  for skname  in soup.findAll('p',    class_='item2')     ]
+        skdesc    = [skdesc.get_text()  for skdesc  in soup.findAll('p',    class_='msg')       ]
+        sk1name   = [sk1name.get_text() for sk1name in soup.findAll('p',    class_='item1_1')   ]
+        sk1desc   = [sk1desc.get_text() for sk1desc in soup.findAll('p',    class_='item1_2')   ]
+        sk1stat   = [sk1stat.get_text() for sk1stat in soup.findAll('div',  class_='item3')     ]
+        sk2name   = [sk2name.get_text() for sk2name in soup.findAll('p',    class_='item1_1')   ]
+        sk2desc   = [sk2desc.get_text() for sk2desc in soup.findAll('p',    class_='item1_2')   ]
+        sk2stat   = [sk2stat.get_text() for sk2stat in soup.findAll('div',  class_='item3')     ]
+        sk3name   = [sk3name.get_text() for sk3name in soup.findAll('p',    class_='item1_1')   ]
+        sk3desc   = [sk3desc.get_text() for sk3desc in soup.findAll('p',    class_='item1_2')   ]
+        sk3stat   = [sk3stat.get_text() for sk3stat in soup.findAll('div',  class_='item3')     ]
         df = pd.append(
             [
                 sk,
@@ -137,7 +143,7 @@ def savestats(df):
 # %%
 def loadstats(Valkyrie_pkl):
     print('Now Loading pickle file to dataframe')
-    df = pd.read_pickle(valkyries_skills.pkl)
+    df_vskills = pd.read_pickle("valkyries_skills.pkl")
     print('Dataframe loading completed!')
     return df_vskills
 
@@ -146,10 +152,14 @@ def loadstats(Valkyrie_pkl):
 def refreshall(urls):
     print('Refreshing Web Content...')
     souplist = loadweb(urls)
-    df_vstats = soup2stats(souplist)
+    df_vskills = soup2skills(souplist)
     return df_vskills.to_excel('valkyrie_skills.xlsx')
     return savesoup(souplist)
     return savestats(df_vstats)
     return print('Task Completed!')
 
+#%%
+vsk = refreshall(urls)
+vsk
 
+# %%
