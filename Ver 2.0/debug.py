65d4b8879038a614c73dc7b26155a60b37acea9a
Eden MUI <emporius@Edens-MacBook-Pro.local>
2020-07-19 21:59:15 +0800

    --- Auto-Git Commit ---

--- Ver 2.0/debug.py
+++ Ver 2.0/debug.py
@@ -18,7 +18,8 @@ import pprint
 
 # %%
 from_page   = 1
-to_page       = range(1,2)
+to_page     = 2
+pages       = range(from_page,to_page+1)
 url_part    = 'https://3rdguide.com/web/valk/detail?id='
 urls        = [url_part + str(page) for page in pages]
 souplist    = []
@@ -34,27 +35,34 @@ def loadweb(urls):
         soup = BeautifulSoup(html.decode('utf-8'),'html.parser')
         souplist.append(soup)
         time.sleep(np.random.randint(2,5))
-    return enumerate(souplist,start=
+    enumerate(souplist,start=from_page)
+    return souplist
 
-#%%
-enumerate??
-    
 #%%
 pp = pprint.PrettyPrinter()
 souplist = loadweb(urls)
 
 #%%
+df = pd.DataFrame()
 for soup in souplist:
+    dic = {}
+    dic.update(enumerate(soup.stripped_strings))
+    name = dic.get(6)
     categories = [category.get_text() for category in soup.findAll('p',class_='item1')]
-    major_name = [category.get_text() for category in soup[0].findAll('p',class_='item2')]
+    major_name = [category.get_text() for category in soup.findAll('p',class_='item2')]
     string1 = str()
-    major_detail = [string1 + category.get_text() for category in soup[0].findAll('p',class_='msg')]
-    minor_name = [category.get_text() for category in soup[0].findAll('p',class_='item1_1')]
+    major_detail = [string1 + category.get_text() for category in soup.findAll('p',class_='msg')]
+    minor_name = [category.get_text() for category in soup.findAll('p',class_='item1_1')]
     string2 = str()
-    minor_detail = [string2 + category.get_text() for category in soup[0].findAll('div',class_='item3')]
+    minor_detail = [string2 + category.get_text() for category in soup.findAll('div',class_='item3')]
     zip1 = zip(major_name,major_detail)
     zip2 = zip(minor_name,minor_detail)
-    dic1 = {zip1}
-    dic2 = {zip2}
+    df1 = pd.DataFrame.from_dict(dic1,orient='columns')
+    df2 = pd.DataFrame.from_dict(dic2,orient='columns')
+    df3 = pd.concat([df1,df2],axis=0,ignore_index=True)
+    df = df.append(df3)
 
+#%%
+df
 
+# %%
