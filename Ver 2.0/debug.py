4d4d70d179c75e2a9e3e9db347e0ac7de5cf7486
Eden MUI <emporius@Edens-MacBook-Pro.local>
2020-07-19 21:29:05 +0800

    --- Auto-Git Commit ---

--- Ver 2.0/debug.py
+++ Ver 2.0/debug.py
@@ -17,7 +17,8 @@ import pprint
 
 
 # %%
-pages       = range(1,2)
+from_page   = 1
+to_page       = range(1,2)
 url_part    = 'https://3rdguide.com/web/valk/detail?id='
 urls        = [url_part + str(page) for page in pages]
 souplist    = []
@@ -33,71 +34,27 @@ def loadweb(urls):
         soup = BeautifulSoup(html.decode('utf-8'),'html.parser')
         souplist.append(soup)
         time.sleep(np.random.randint(2,5))
-    return souplist
+    return enumerate(souplist,start=
+
+#%%
+enumerate??
     
 #%%
 pp = pprint.PrettyPrinter()
-soup = loadweb(urls)
+souplist = loadweb(urls)
 
 #%%
-categories = [category.get_text() for category in soup[0].findAll('p',class_='item1')]
-pp.pprint(categories)
-print(len(categories))
-#%%
-major_name = [category.get_text() for category in soup[0].findAll('p',class_='item2')]
-pp.pprint(major_name)
-print(len(major_name))
-
-#%%
-string1 = str()
-major_detail = [string1 + category.get_text() for category in soup[0].findAll('p',class_='msg')]
-pp.pprint(major_detail)
-print(len(major_detail))
-#%%
-minor_name = [category.get_text() for category in soup[0].findAll('p',class_='item1_1')]
-pp.pprint(minor_name)
-print(len(minor_name))
-
-#%%
-string2 = str()
-minor_detail = [string2 + category.get_text() for category in soup[0].findAll('div',class_='item3')]
-pp.pprint(minor_detail)
-print(len(minor_detail))
-
-#%%
-for string in soup[0].stripped_strings:
-    lst.append(string)
-
-#%%
-skills = [skill.get_text() for skill in soup[0].findAll('p',class_='item2')]
-index = []
-print(cats)
-for cat in cats:
-    position = lst.index(cat)
-    index.append(position)
-
-# %%
-print(index)
-#%%
-dic = {}
-for r in range(0,5):
-    dic.update(
-        {
-            lst[index[r]]:
-            lst[
-                index[r]+1:
-                int(index[r+1])-1
-            ]
-        }
-    )
-
-#%%
+for soup in souplist:
+    categories = [category.get_text() for category in soup.findAll('p',class_='item1')]
+    major_name = [category.get_text() for category in soup[0].findAll('p',class_='item2')]
+    string1 = str()
+    major_detail = [string1 + category.get_text() for category in soup[0].findAll('p',class_='msg')]
+    minor_name = [category.get_text() for category in soup[0].findAll('p',class_='item1_1')]
+    string2 = str()
+    minor_detail = [string2 + category.get_text() for category in soup[0].findAll('div',class_='item3')]
+    zip1 = zip(major_name,major_detail)
+    zip2 = zip(minor_name,minor_detail)
+    dic1 = {zip1}
+    dic2 = {zip2}
 
 
-# %%
-duc
-
-# %%
-dic
-
-# %%
