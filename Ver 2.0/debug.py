73d057f2b4ac1ed951af8258cab5e189d1082475
Eden MUI <emporius@Edens-MacBook-Pro.local>
2020-07-19 23:29:45 +0800

    --- Auto-Git Commit ---

--- Ver 2.0/debug.py
+++ Ver 2.0/debug.py
@@ -18,7 +18,7 @@ import pprint
 
 # %%
 from_page   = 1
-to_page     = 100
+to_page     = 60
 pages       = range(from_page,to_page+1)
 url_part    = 'https://3rdguide.com/web/valk/detail?id='
 urls        = [url_part + str(page) for page in pages]
@@ -28,17 +28,16 @@ df          = pd.DataFrame()
 path        = os.path.abspath(os.getcwd())
 
 #%%
+souplist = []
 def loadweb(urls):
     progress = from_page
     for url in urls:
         html = urlopen(url).read()
         soup = BeautifulSoup(html.decode('utf-8'),'html.parser')
-        souplist    = []
         souplist.append(soup)
         print(str(progress) + ' of ' + str(to_page) + ' pages loading...')
         time.sleep(np.random.randint(1,3))
         progress = progress + 1
-    enumerate(souplist,start=from_page)
     return souplist
 
 #%%
@@ -51,21 +50,27 @@ df = pd.DataFrame()
 for soup in souplist:
     dic = {}
     dic.update(enumerate(soup.stripped_strings))
-    name = dic.get(6)
-    categories = [category.get_text() for category in soup.findAll('p',class_='item1')]
-    major_name = [category.get_text() for category in soup.findAll('p',class_='item2')]
-    string1 = str()
-    major_detail = [string1 + category.get_text() for category in soup.findAll('p',class_='msg')]
-    minor_name = [category.get_text() for category in soup.findAll('p',class_='item1_1')]
-    string2 = str()
-    minor_detail = [string2 + category.get_text() for category in soup.findAll('div',class_='item3')]
-    major = pd.DataFrame([major_name,major_detail],index=['技能','描述'])
-    minor = pd.DataFrame([minor_name[1:-1],minor_detail],index=['技能','描述'])
-    concat = pd.concat([major,minor],axis=1).transpose()
-    concat['女武神'] = name
-    concat['Page'] = from_page
-    df = df.append(concat).dropna()
-df = df.set_index('女武神')
+    if dic.get(6) == None:
+        pass
+    else:
+        name = dic.get(6)
+        categories = [category.get_text() for category in soup.findAll('p',class_='item1')]
+        major_name = [category.get_text() for category in soup.findAll('p',class_='item2')]
+        string1 = str()
+        major_detail = [string1 + category.get_text() for category in soup.findAll('p',class_='msg')]
+        minor_name = [category.get_text() for category in soup.findAll('p',class_='item1_1')]
+        string2 = str()
+        minor_detail = [string2 + category.get_text() for category in soup.findAll('div',class_='item3')]
+        major = pd.DataFrame([major_name,major_detail],index=['技能','描述'])
+        minor = pd.DataFrame([minor_name[1:-1],minor_detail],index=['技能','描述'])
+        concat = pd.concat([major,minor],axis=1).transpose()
+        concat['女武神'] = name
+        concat['Page'] = from_page
+        concat['描述'] = concat['描述'].astype(str).str.strip()
+        from_page = from_page + 1
+        df = df.append(concat)
+    df = df.set_index('女武神')
+    df.dropna()
 
 #%%
 df.to_excel('valkyrie_skills.xlsx')
@@ -73,5 +78,6 @@ df.to_excel('valkyrie_skills.xlsx')
 # %%
 df
 
-
+#%%
+len(souplist)
 # %%
